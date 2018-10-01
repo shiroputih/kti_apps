21ef4cd76152b296cd470ee5eec2c40e79cce866
shiro sinosuke <kristiantopurwokoadi@gmail.com>
2018-10-01 10:22:11 +0700

    Update Footer.php

--- Footer.php
+++ Footer.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer">
     <div class="container">
         <div class="text-center">
-            <small>Copyright © Medical Education Unit FK UKDW 2018</small>
+            <small>Copyright © Fakultas Kedokteran Universitas Kristen Duta Wacana 2018</small>
         </div>
     </div>
 </footer>
