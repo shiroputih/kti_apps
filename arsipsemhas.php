78611475b6cbeb25525824db81de8aeeaf33d890
shiro sinosuke <kristiantopurwokoadi@gmail.com>
2018-10-03 20:08:37 +0700

    + add delete feature in semhas arsip

--- arsipsemhas.php
+++ arsipsemhas.php
@@ -69,8 +69,10 @@
 								<thead>
 									<tr>
 										<th>NIM</th>
-										<th>Semester</th>
-										<th>File Pdf</th>
+		                                <th>Nama</th>
+		                                <th>Semester</th>
+		                                <th>File Pdf</th>
+		                                <th>Action</th
 									</tr>
 								</thead>
 								<tbody>
@@ -80,14 +82,20 @@
 									if ($result->num_rows > 0) {
         // output data of each row
 										while ($row = $result->fetch_assoc()) {
-											?>
+											echo"
 											<tr>
-												<td><?php echo $row['nim'];?></td>
-												<td><?php echo $row['nama'];?></td>
-												<td><a href="<?php echo $row['semhas_filepdf'];?>" target="_blank"><img src="icons/pdficon.png" width="30px" height="30px"></a></td>
-											</tr>
-
-											<?php
+												<td>$row[nim]</td>
+												<td>$row[nama]</td>
+												<td>$row[semester]</td>
+												<td><a href=$row[semhas_filepdf] target='_blank'><img src='icons/pdficon.png' width='30px' height='30px'></a></td>
+												<td>
+												<a id ='deletearsipsemhas' data-idarsipsemhas=$row[id_arsipsemhas] data-toggle='modal' data-target='#deletearsipsemhasmodal'>
+												<button type='button' class='btn btn-danger btn-circle btn-sm'>
+												<i class='fa fa-times'></i>
+												</button>
+											</a>
+											</td>
+											</tr>";
 										}
 									}
 									?>
@@ -107,6 +115,58 @@
 <?php
 @include("footer.php");
 ?>
+<div class="modal fade" id="deletearsipsemhasmodal" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title">Delete Arsip Berita Acara Seminar Hasil</h4>
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
+			</div>
+			<div class="modal-body" style="background-color: #c82333;">
+				<div class="card-body">
+					<form method="post" enctype="multipart/form-data">
+						<input type="hidden" class="form-control" name="delnmsemhas" id="delnmsemhas" type="text"
+						aria-describedby="nameHelp">
+						<div id="datahapusarsipsemhas"></div>
+						<br>
+						<button type="submit" class="btn btn-info btn-sm" data-dismiss="modal" data-dismiss="modal">NO</button>
+						<button type="submit" name="DeleteDataBAsemhas" class="btn btn-danger btn-sm" >YES</button>
+					</form>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+<!-- hapus arsip berita acara proposal -->
+<script type="text/javascript">
+	$(document).on("click", "#deletearsipsemhas", function () {
+		var id_arsippsemhas = $(this).data('idarsipsemhas');
+		
+		$('#delnmsemhas').val(id_arsippsemhas);
+	});
+</script> 
+
+<script type="text/javascript">
+	$(document).ready(function()
+	{
+		$('#deletearsipsemhasmodal').on('show.bs.modal', function (e) 
+		{
+			var idarsipsemhas = $(e.relatedTarget).data('idarsipsemhas');
+			//menggunakan fungsi ajax untuk pengambilan data
+            $.ajax(
+            {
+            	type : 'post',
+            	url : 'hapusBAsemhas.php',
+            	data :  'id='+ idarsipsemhas,
+            	success : function(data)
+            	{
+                    $('#datahapusarsipsemhas').html(data);//menampilkan data ke dalam modal
+                }
+            });
+        });
+	});
+</script> 
 
 <?php
 if(isset($_POST['uploadsemhas']))
@@ -127,6 +187,15 @@ if(isset($_POST['uploadsemhas']))
      		 echo "<meta http-equiv='refresh' content='0'>";
   	 	}
   	 }
+}
+?>
+<?php
+if (isset($_POST['DeleteDataBAsemhas'])) {
+        //update tabel mahasiswa
+	$sql = "DELETE FROM arsipsemhas WHERE id_arsipsemhas = '$_POST[delnmsemhas]'";
+	if (mysqli_query($conn, $sql)) {
+		echo "<meta http-equiv='refresh' content='0'>";
+	}
 }
 ?>
 <!-- Bootstrap core JavaScript
